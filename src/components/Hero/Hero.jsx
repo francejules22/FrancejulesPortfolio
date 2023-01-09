f61b3b29f41c7fd7e6cf09d5b122cc59767754b0
Francis Jules <francisjulescelesteespartero@gmail.com>
2023-01-09 23:01:30 +0800

    Updated Resume Again

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
                         <p>Studied in ISAT U (Iloilo Science and Technology University) taking Computer Science Degree, and a passionate self-taught web developer.</p>
 
                         <div className="mt-4 hero__btn d-flex align-items-center gap-4">
-                           <button className="hire_btn"><a href="https://drive.google.com/file/d/1SnDiMjZPNBHIayfNqU4OVQnH0vXrMixY/view?usp=sharing"  rel="noreferrer" target="_blank">Resume</a></button> 
+                           <button className="hire_btn"><a href="https://drive.google.com/file/d/1G8YXCHe95vAs3dFGVdmOa9XT92f930IE/view?usp=sharing"  rel="noreferrer" target="_blank">Resume</a></button> 
                            <button className="contact_btn"><a href="#contact">Contact</a></button>
                         </div>
                     </div>
